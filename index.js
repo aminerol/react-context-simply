f703d5bc82729dd73fec801c9b908a0b7aef14df
amine <zazamine8@gmail.com>
2019-11-09 02:26:10 +0000

    fixing the hook middlware to certain action(s), it was running only one from the middlwares add cheks if middlwares passed to createStateContext or not

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ import React, { createContext, useContext, useReducer, useCallback } from 'react
 
 const augmentDispatch = (state, dispatch) => (input) => typeof input === 'function' ? input(state, dispatch) : dispatch(input);
 export default function createStateContext(initialState, reducer, actions, middleware) {
-    const middlewares = Array.isArray(middleware) ? middleware : [middleware]
+    const middlewares = Array.isArray(middleware) ? middleware : (middleware && [middleware])
     const StateContext = createContext([initialState, () => null]);
     const StateProvider = ({children}) => {
-        const [state, dispatch] = middlewares.reduce((result, mw) => {
-          const [st, agg] = useReducer(reducer, initialState)
+        const chain = []
+        const [state, dispatch] = middlewares ? middlewares.reduce(([st, agg], mw) => { 
           const newDis = action => {
             const types = Array.isArray(mw.action) ? mw.action : [mw.action]
             if(types.includes(action.type) || types.includes('*')){
-              return mw ? mw.middleware([st, agg]) : agg
+              let [_, next] = mw.middleware([state, agg])
+              return next(action)
             }else{
               return agg(action)
             }
           }
+          chain.push(newDis)
           return [st, newDis]
-        }, []);
-        const enhancedDispatch = augmentDispatch(state, dispatch)
+        }, useReducer(reducer, initialState)) : useReducer(reducer, initialState)
+        const composedDispatch = chain.length > 0 ? chain.reduce((result, next) => async (action) => action && await result(await next(action)), dispatch) : dispatch
+        const enhancedDispatch = augmentDispatch(state, composedDispatch)
         const newActions = Object.keys(actions).reduce((result, action) => {
           result[action] = useCallback((payload) =>  enhancedDispatch(actions[action](payload)), [])
           return result;
