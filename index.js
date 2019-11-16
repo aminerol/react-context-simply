9f067e9d88c5f0e3292958652a40040346b8cbae
amine <zazamine8@gmail.com>
2019-11-16 20:51:22 +0000

    add spread operator so it pass all args passed to actions

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import React, { createContext, useContext, useReducer, useCallback } from 'react';
 
 const augmentDispatch = (state, dispatch) => (input) => typeof input === 'function' ? input(state, dispatch) : dispatch(input);
+
+/**
+   * creates the provider as well as the hook that returns store and actions
+   *
+   * @param {T} initialState The initial State to begin with
+   * @param {Reducer<T>} reducer it describes how an action transforms the state into the next state
+   * @param {IActions} actions sets of actions that holds information that send data to your store
+   * @param {IMiddleware | IMiddleware[]} middleware array of objects, each contain the middleware function and action that will be hooked to it
+   * @returns {IStateContext<T>} it return basically our provider also a hook function that will return our store and actions
+   * @public
+   */
 export default function createStateContext(initialState, reducer, actions, middleware) {
     const middlewares = Array.isArray(middleware) ? middleware : (middleware && [middleware])
     const StateContext = createContext([initialState, () => null]);
@@ -24,7 +35,7 @@ export default function createStateContext(initialState, reducer, actions, middl
         const composedDispatch = chain.length > 0 ? chain.reduce((result, next) => async (action) => action && await result(await next(action)), dispatch) : dispatch
         const enhancedDispatch = augmentDispatch(state, composedDispatch)
         const newActions = Object.keys(actions).reduce((result, action) => {
-          result[action] = useCallback((payload) =>  enhancedDispatch(actions[action](payload)), [])
+          result[action] = useCallback((...payload) =>  enhancedDispatch(actions[action](...payload)), [])
           return result;
         }, {});
         return <StateContext.Provider value={[state, newActions]}>{children}</StateContext.Provider>
